6f887e3f90c0c5a4c9cb9b34125962ceaca6c6fc
ewanaszydlowska <ewanasz@gmail.com>
2018-01-25 12:33:10 +0100

    selection search algorithm update

--- src/algorithms/SelectionSort.java
+++ src/algorithms/SelectionSort.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int[] testArray = {7, 9, 4, 8, 2, 11, 17, 1};
-		selectionSort(testArray);
+		int[] testArray = { 7, 9, 4, 8, 2, 11, 17, 1 };
+		// selectionSort(testArray);
+		selectionSortAllInOne(testArray);
 		System.out.println(Arrays.toString(testArray));
 	}
-	
+
+	//version 1 with 3 functions
 	static void swap(int[] arr, int firstIndex, int secondIndex) {
 		int temp = arr[firstIndex];
 		arr[firstIndex] = arr[secondIndex];
 		arr[secondIndex] = temp;
 	}
-	
+
 	static int indexOfMinimun(int[] arr, int startIndex) {
 		int minValue = arr[startIndex];
 		int minIndex = startIndex;
@@ -27,14 +29,28 @@ public class SelectionSort {
 		}
 		return minIndex;
 	}
-	
+
 	static void selectionSort(int[] arr) {
 		int value;
 		for (int i = 0; i < arr.length; i++) {
 			value = indexOfMinimun(arr, i);
 			swap(arr, i, value);
 		}
-		
+
 	}
-		
+
+	//version 2 with one function
+	static void selectionSortAllInOne(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int index = i;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[j] < arr[index]) {
+					index = j;
+				}
+			}
+			int smallerNum = arr[index];
+			arr[index] = arr[i];
+			arr[i] = smallerNum;
+		}
+	}
 }
